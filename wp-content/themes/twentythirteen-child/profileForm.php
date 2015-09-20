9816dd59216fde75d9935b4e234eac3837a49a5d
kevin578 <kevinbriggs1@gmail.com>
2015-09-20 10:46:13 -0400

    login and logout finished and stylized.

--- wp-content/themes/twentythirteen-child/profileForm.php
+++ wp-content/themes/twentythirteen-child/profileForm.php
@@ -1,5 +1,13 @@
+<div id = "profileForm" class = "login_profile"> 
+
+<p>😀</p>
 <?php
-echo "Hello $name";
+echo "Hello $name!";
 ?>
 <br>
+
+<a href="profile">Profile</a><br>
 <a href = "wp-content/themes/twentythirteen-child/logout.php">Logout</a>
+
+
+</div>
