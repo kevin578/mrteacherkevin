e1da992ae89ef94741cb1fc55a5d28c321f26cf3
kevin578 <kbriggs@aapl000894.local>
2016-09-20 20:29:07 -0400

    locations working

--- wp-content/plugins/job-listing/wp-job-listing.php
+++ wp-content/plugins/job-listing/wp-job-listing.php
@@ -15,7 +15,6 @@ function mtk_register_post_type(){
 
 	$singular = 'Job Listing';
 	$plural = 'Job Listings';
-	$slug = str_replace( ' ', '_', strtolower( $singular ) );
 
 		$labels = array(
 		'name' 				=> $plural,
@@ -67,9 +66,45 @@ function mtk_register_post_type(){
 	        	'custom-fields' 
 	        )
 	);
-	register_post_type( $slug, $args );
+	register_post_type( 'job_listing', $args );
 }
 add_action( 'init', 'mtk_register_post_type');
+
+
+function mtk_register_taxonomy() {
+	$singular = 'Location';
+	$plural = 'Locations';
+	$slug = str_replace( ' ', '_', strtolower( $singular ) );
+	$labels = array(
+		'name'                       => $plural,
+        'singular_name'              => $singular,
+        'search_items'               => 'Search ' . $plural,
+        'popular_items'              => 'Popular ' . $plural,
+        'all_items'                  => 'All ' . $plural,
+        'parent_item'                => null,
+        'parent_item_colon'          => null,
+        'edit_item'                  => 'Edit ' . $singular,
+        'update_item'                => 'Update ' . $singular,
+        'add_new_item'               => 'Add New ' . $singular,
+        'new_item_name'              => 'New ' . $singular . ' Name',
+        'separate_items_with_commas' => 'Separate ' . $plural . ' with commas',
+        'add_or_remove_items'        => 'Add or remove ' . $plural,
+        'choose_from_most_used'      => 'Choose from the most used ' . $plural,
+        'not_found'                  => 'No ' . $plural . ' found.',
+        'menu_name'                  => $plural,
+	);
+	$args = array(
+            'hierarchical'          => true,
+            'labels'                => $labels,
+            'show_ui'               => true,
+            'show_admin_column'     => true,
+            'update_count_callback' => '_update_post_term_count',
+            'query_var'             => true,
+            'rewrite'               => array( 'slug' => $$slug ),
+        );
+	register_taxonomy( 'location', 'job_listing', $args );
+}
+add_action( 'init', 'mtk_register_taxonomy' );
 
 
 ?>
