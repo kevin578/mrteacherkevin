bdcb1793cb255be6b63a850c622fbb1829c503ed
kevin578 <kbriggs@MA000894-KBRIGGS.hsd1.ct.comcast.net>
2016-11-25 13:28:40 -0500

    front end of content area finished

--- wp-content/plugins/mtk-challenge-page/js/admin_input.js
+++ wp-content/plugins/mtk-challenge-page/js/admin_input.js
@@ -1,18 +1,70 @@
-var skill_input_id = 0;
 
+//sets arrays with one instance of a skill and one of a time. Third one sets array with skill for checkboxes and the plus and minus
+var skill_input = ['<input type = "text" class = "video_time_left" placeholder = "Skill">'];
+var time_input = ['<input type = "text" class = "video_time_right" placeholder = "Time">'];
+var check_input = ['<input type = "text" class = "check_input_box" placeholder="Skill to have completed">\
+					<input type = "button" value = "+" class = "plus_button" onclick = "add_check_input();">\
+					<input type = "button" value = "-" onclick="remove_check_input();")>'];
+
+
+//displays one row on load
 window.onload = function() {
 
-jQuery( "#skill_inputs" ).append(skill_input);
+render_skill_input();
+render_check_input();
 
 }
 
 
-	var skill_input = 
-'<input type = "text" class = "video_time_left" placeholder = "Skill">\
-<input type = "text" class = "video_time_right" placeholder = "Time">'
-
-
+//pushes another item to the arrays when the plus button is press and calls the render function
 function add_skill_input() {
+skill_input.push('<input type = "text" class = "video_time_left" placeholder = "Skill">');
+time_input.push('<input type = "text" class = "video_time_right" placeholder = "Time">');
 
-	jQuery( "#skill_inputs" ).append(skill_input);
+render_skill_input();
 }
+
+function add_check_input() {
+	check_input.push('<input type = "text" class = "check_input_box" placeholder="Skill to have completed">');
+	render_check_input();
+}
+//removes skill from array and renders it again
+function remove_skill_input() {
+	skill_input.pop();
+	time_input.pop();
+
+	render_skill_input();
+}
+
+function remove_check_input() {
+	check_input.pop();
+
+	render_check_input();
+}
+
+
+//diplays the arrays
+function render_skill_input() {
+	jQuery("#skill_inputs").html("");
+
+	for (var i = 0; i < skill_input.length; i++) {
+		jQuery("#skill_inputs").append(skill_input[i]);
+		jQuery("#skill_inputs").append(time_input[i]);
+	}
+}
+
+function render_check_input() {
+	jQuery("#check_input").html("");
+
+	for (var i = 0; i < check_input.length; i++) {
+		jQuery("#check_input").append(check_input[i]);
+	}
+}
+
+
+
+
+
+
+
+
--- wp-content/plugins/mtk-challenge-page/mtk_challenge_box1_form.php
+++ wp-content/plugins/mtk-challenge-page/mtk_challenge_box1_form.php
@@ -1,4 +1,4 @@
-<html>
+
 
 <div class = "mtk_challenge_input">
 
@@ -6,19 +6,30 @@
 <h1>What will I be making?</h1>
 <textarea id = 'mtk_challengebox1' rows = '3'></textarea>
 
-<h1>Okay, how can I make this?</h1>
+<h1>How can I make this?</h1>
 <textarea id = 'mtk_challengebox1' rows = '3'>
 	In order to make your own version of this you can follow along with the video below. Remember when you see a change you can make to your project, pause the video and go do it. Don’t watch the whole video and try to remember it!
 </textarea>
 
 
 <h1>What if I want to find a specific skill in the video?</h1>
+
+
 <p>Here is a list of skills with the time they appear in the video.</p>
-<input type = "button" value = "+" id = "plus_button" onclick = "add_skill_input();">
-<input type = "button" value = "-">
+<input type = "button" value = "+" class = "plus_button" onclick = "add_skill_input();">
+<input type = "button" value = "-" onclick="remove_skill_input();")>
 
 
 <div id = "skill_inputs"></div>
+
+
+<h1>Okay, I think I’m finished. What can I check to make sure?</h1>
+
+<div id = "check_input"></div>
+
+<h1>You can find everything you need in the video below:</h1>
+<input type = "text" id = "video_embed_url">
+
 
 </div>
 </html>
