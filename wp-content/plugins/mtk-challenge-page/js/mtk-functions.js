9d72c56acba3fb7598f57fdb7e5034a693e0fe26
kevin578 <kbriggs@aapl000894.local>
2017-02-21 12:05:36 -0500

    finished quiz still need to ajax it

--- wp-content/plugins/mtk-challenge-page/js/mtk-functions.js
+++ wp-content/plugins/mtk-challenge-page/js/mtk-functions.js
@@ -1,7 +1,9 @@
 
+//All of the functions that get called in during ajax and the buttons being pressed
 
 
 
+//Gets the check marks ready that show up on the main input page.
 function render_checks() {
 
 hide_button(box1_check, "#box1_check", "#box1_title", "#box1_content");
@@ -13,7 +15,7 @@ hide_button(box5_check, "#box5_check", "#video_title", "#video_embed_url");
 
 
 
-//functions
+
 
 function hide_button(check_id, click_id, title, content) {
 
@@ -189,54 +191,160 @@ function render_extra_boxes() {
 
 //quiz
 
+
+jQuery( document ).ready(function() {
+
+	var mtk_quiz_questions = [];
+	
+
+	jQuery("#quiz_text").click(function(){
+		add_text_question();
+	});
+
+	jQuery("#quiz_mc").click(function(){
+		add_mc_question();
+	});	
+
+});
+
+
+
+function render_question(){
+
+
+ 
+
+//clears all of the html away so it can be repopulated with the arrays
+
+	jQuery("#mtk_quiz_questions").html("");
+
+
+//for each question in the array, it will create...
+	for(var i = 0; i < mtk_quiz_questions.length; i++) {
+		var question_num = i + 1;
+		
+		//the box the indiviual questions are contained in 
+		jQuery("#mtk_quiz_questions").append('<div id = "mtk_question_container_'+i+'" class = "mtk_question_container"></div>');
+		//the x block to delete the questions 
+		jQuery("#mtk_question_container_" + i).append('<input type = "button" id = "remove_question_'+question_num+'" class = "remove_question" value = "x" onclick = "remove_question('+ i +')">');
+		//the question title
+		jQuery("#mtk_question_container_" + i).append('<div class = "quiz_titles">Question '+ question_num +'</div>');
+		//actual text of the question
+		jQuery("#mtk_question_container_" + i).append('<input type = "text" class = "question_text" id = "question' + question_num + '" value = "'+ mtk_quiz_questions[i].question+'">');
+
+		//if it's a text quesitontion it makes the answer title and text input box
+		if (mtk_quiz_questions[i].type == "text") {
+			
+
+			jQuery("#mtk_question_container_" + i).append('<div class = "answer_titles"><em>Answer</em></div>');
+			jQuery("#mtk_question_container_" + i).append('<input type = "text" class = "answer_text" id = "answer_'+ question_num+'" value = "'+ mtk_quiz_questions[i].answer+'">');
+		}
+		//if it's multiple choice it goes to the choices array loctated at mtk_quiz_questions[i].choices
+		else if (mtk_quiz_questions[i].type == "mc") {
+			
+			//first adds the plus and minus button
+			jQuery("#mtk_question_container_" + i).append('<input type = "button" value = "+" class = "plus_button" onclick = "add_mc_choice('+i+');" id = "plus_mc">');
+			jQuery("#mtk_question_container_" + i).append('<input type = "button" value = "-" class = "minus_button" onclick = "remove_mc_choice('+i+','+x+');" id = "plus_mc"><br>');			
+			
+			//for each choice it creates the radio button and box to write the choice in
+			for (var x = 0; x < mtk_quiz_questions[i].choices.length; x++){
+
+				jQuery("#mtk_question_container_" + i).append('<input type = "radio" name = "question'+ question_num +'" id = "mc_radio">');
+				jQuery("#mtk_question_container_" + i).append('<input type = "text" value = "'+mtk_quiz_questions[i].choices[x]+'" class = "mc_choice" id = "mc_choice_'+x+'"><br>');
+			}
+		}
+
+	}
+}
+
 function add_text_question(){
 
 	var question = {
 		id: mtk_quiz_questions.length,
-		type: "Text",
+		type: "text",
 		question: "",
 		answer:""
 	} 
-
 	mtk_quiz_questions.push(question); 
-	render_text_question();
+	record_quiz();
+	render_question();
 }
 
 
-
-function render_text_question(){
-
-	jQuery("#mtk_quiz_questions").html("");
-
-	for(var i = 0; i < mtk_quiz_questions.length; i++) {
-		var question_num = i + 1;
-		
-
-		jQuery("#mtk_quiz_questions").append('<div id = "mtk_question_container_'+i+'" class = "mtk_question_container"></div>');
-		jQuery("#mtk_question_container_" + i).append('<input type = "button" id = "remove_question_'+question_num+'" class = "remove_question" value = "x" onclick = "remove_question('+ i +')">');
-		jQuery("#mtk_question_container_" + i).append('<div class = "quiz_titles">Question '+ question_num +'</div>');
-		jQuery("#mtk_question_container_" + i).append('<input type = "text" class = "question_text" id = "question' + question_num + '">');
-		jQuery("#mtk_question_container_" + i).append('<div class = "answer_titles"><em>Answer</em></div>');
-		jQuery("#mtk_question_container_" + i).append('<input type = "text" class = "answer_text" id = "answer_'+ question_num+'">')
+function add_mc_question(){
+		var question = {
+		id: mtk_quiz_questions.length,
+		type: "mc",
+		question: "",
+		choices:["",""],
+		answer:""
 	}
+
+		mtk_quiz_questions.push(question); 
+		record_quiz();
+		render_question();
 }
+
+function add_mc_choice(question_num) {
+	mtk_quiz_questions[question_num].choices.push("");
+	record_quiz();
+	render_question();
+
+}
+
+function remove_mc_choice(question_num, choice_num) {
+	mtk_quiz_questions[question_num].choices.splice(choice_num, 1);
+	record_quiz();
+	render_question();
+
+}
+
+
 
 function remove_question(value){
+	record_quiz();
 	mtk_quiz_questions.splice(value, 1);
-	render_text_question();
+	render_question();
+}
+
+function record_quiz() {
+		for(var i = 0; i < mtk_quiz_questions.length; i++) {
+
+		var question = "#question" + parseInt(i + 1);
+		var question_val = jQuery(question).val();
+
+		if (typeof question_val != "undefined") {
+					
+			mtk_quiz_questions[i].question = question_val;
+
+
+
+			if (mtk_quiz_questions[i].type == "text") {
+				var answer = "#answer_" + parseInt(i + 1);
+				var answer_val = jQuery(answer).val();	
+				
+				if (typeof answer_val != "undefined"){
+				mtk_quiz_questions[i].answer = answer_val;
+			}
+			}
+
+			else if (mtk_quiz_questions[i].type == "mc") {
+				for (var x = 0; x < mtk_quiz_questions[i].choices.length; x++) {
+					var choice = "#mc_choice_" + parseInt(x);
+					var choice_val = jQuery(choice).val();	
+				
+				if (typeof choice_val != "undefined"){
+					mtk_quiz_questions[i].choices[x] = choice_val; 
+				}
+			}
+		}
+}
 }
 
 
+}
 
-/*<div class = "quiz_titles" id = "question1_title">Question 1</div>
-<input type = "text" class = "question_text" id = "question_1">
 
-<div class = "quiz_titles" id = "answer1_title">Answer 1</div>
-<input type = "text" class = "question_text" id = "answer_1">
-
-'<input type = "text" class = "question_text" id = "question_' + i + 1'>'
-
-*/
 
 
 
--- wp-content/plugins/mtk-challenge-page/mtk-challenge-page.php
+++ wp-content/plugins/mtk-challenge-page/mtk-challenge-page.php
@@ -30,7 +30,6 @@ wp_enqueue_script('mtk-functions-js', plugins_url( 'js/mtk-functions.js', __FILE
 wp_enqueue_script( 'jquery' );
 wp_enqueue_script('ajax-js', plugins_url( 'js/mtk-ajax.js', __FILE__ ), array('mtk-functions-js'), true );
 wp_enqueue_script('menu-js', plugins_url( 'js/menu.js', __FILE__ ), array('mtk-functions-js','ajax-js'), true );
-wp_enqueue_script('mtk_quiz-js', plugins_url( 'js/mtk-quiz-input.js', __FILE__), array('mtk-functions-js', 'ajax-js'), true );
 wp_localize_script( 'mtk-functions-js', 'wpApiSettings', array( 'root' => esc_url_raw( rest_url() ), 'nonce' => wp_create_nonce( 'wp_rest' ) ) );
 wp_localize_script('ajax-js', 'get_post_id', array( 'post_id'=>$post->ID));
 wp_localize_script('ajax-js', 'mtk_challenge_boxes', $meta_content_1_object);
